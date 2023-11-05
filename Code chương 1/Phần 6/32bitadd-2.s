fb25d8ff693d2d67cd1b215e0a64205c3a922134
Kilcud <128439565+Kilcud@users.noreply.github.com>
2023-11-05 17:00:07 +0700

    Update 32bitadd-2.s

--- Code chương 1/Phần 6/32bitadd-2.s
+++ Code chương 1/Phần 6/32bitadd-2.s
@@ -7,7 +7,7 @@
 Main		
 		LDR		R0, =Value1		    ; Tải địa chỉ giá trị đầu tiên
 		LDR		R1, [R0]		    ; Tải những gì tại địa chỉ đó
-		ADD		R0, R0, #0x4	    ; Điều chỉnh con trỏ
+		ADD		R0, R0, #0x4	    	    ; Điều chỉnh con trỏ
 		LDR		R2, [R0]		    ; Tải những gì tại địa chỉ mới
 		ADD		R1, R1, R2		    ; Cộng
 		LDR		R0, =Result		    ; Tải địa chỉ lưu trữ
